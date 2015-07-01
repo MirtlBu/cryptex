009d4c8535e75d1af2956a4ac0015ab5e7dc2554
mirtlbu <bubuzoglo@gmail.com>
2015-07-01 14:10:41 +0300

    option with few tables

--- bookmark_tab_exchange.php
+++ bookmark_tab_exchange.php
@@ -2,25 +2,104 @@
     <thead>
         <tr>
             <th class="align-left">From</td>
-            <th class="align-right">To</td>
         </tr>
     </thead>
     <tbody>
         <tr>
             <td class="align-left align-left--active td__bookmark--exchange" data-val="webmoney"><span>Webmoney</span></td>
-            <td class="align-right"></td>
         </tr>
         <tr>
             <td class="align-left td__bookmark--exchange" data-val="perfectmoney"><span>Perfectmoney</span></td>
-            <td class="align-right"></td>
         </tr>
         <tr>
             <td class="align-left td__bookmark--exchange" data-val="paypal"><span>Paypal</span></td>
-            <td class="align-right"></td>
         </tr>
         <tr>
             <td class="align-left td__bookmark--exchange" data-val="sberbank"><span>Sberbank</span></td>
-            <td class="align-right"></td>
+        </tr>
+    </tbody>
+</table>
+<table class="table__bookmark table__bookmark--exchange table__bookmark--active" data-val="webmoney">
+    <thead>
+        <tr>
+            <th class="align-right">To</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Paypal</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Sberbank</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Perfectmoney</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Bitcoin</a></td>
+        </tr>
+    </tbody>
+</table>
+<table class="table__bookmark table__bookmark--exchange" data-val="perfectmoney">
+    <thead>
+        <tr>
+            <th class="align-right">To</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Sberbank</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Bitcoin</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Paypal</a></td>
+        </tr>
+    </tbody>
+</table>
+<table class="table__bookmark table__bookmark--exchange" data-val="sberbank">
+    <thead>
+        <tr>
+            <th class="align-right">To</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Webmoney</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Sberbank</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Paypal</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Bitcoin</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Alfabank</a></td>
+        </tr>
+    </tbody>
+</table>
+<table class="table__bookmark table__bookmark--exchange" data-val="paypal">
+    <thead>
+        <tr>
+            <th class="align-right">To</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Webmoney</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Sberbank</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Bitcoin</a></td>
+        </tr>
+        <tr>
+            <td class="align-right td__bookmark--exchange"><a href="#" class="underlined">Alfabank</a></td>
         </tr>
     </tbody>
 </table>
--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,8 @@
 $(function() {
-    String.prototype.times = function(n) {
-        return Array.prototype.join.call({length: n+1}, this);
-    };
+
     console.log('init');
     //пример селекта, инициализация и получение данных аджаксом
     var selectpay = $('#select-pay');
-
-    var exchange = {
-        webmoney:["qiwi", "perfectmoney", "western union", "sberbank"],
-        perfectmoney:["okpay", "webmoney", "paypal", "sberbank"],
-        paypal:["western union", "webmoney", "qiwi", "perfectmoney", "sberbank", "vtb24"],
-        sberbank:["qiwi", "okpay", "webmoney", "western union", "perfectmoney", "paypal"]
-    };
 
     $.ajax({
         url: "/static/js/options.json",
@@ -52,34 +43,16 @@ $(function() {
             $('.bookmark__tab').load('bookmark_tab_best.php');
         }
         else if(dataVal == 'exchange') {
-            $('.bookmark__tab').load('bookmark_tab_exchange.php', function() {
-                $('.table__bookmark--exchange tbody .align-right').each(function(i) {
-                    $(this).html('<a href="" class="underlined">' + exchange.webmoney[i] + '</a>');
-                });
-            });
+            $('.bookmark__tab').load('bookmark_tab_exchange.php');
         }
     });
 
     $('.bookmark').on('click', '.td__bookmark--exchange', function() {
-        var dataVal = $(this).attr('data-val');
-        var td = $('.table__bookmark--exchange tbody .align-right').get();
-        var tr = '<tr><td class="align-left"></td><td class="align-right"></td></tr>';
-        var arr = exchange[dataVal];
-
+        var activeVal = $(this).attr('data-val');
+        $('.table__bookmark--exchange').removeClass('table__bookmark--active');
+        $('.table__bookmark--exchange[data-val=' + activeVal + ']').addClass('table__bookmark--active');
         $('.td__bookmark--exchange').removeClass('align-left--active');
         $(this).addClass('align-left--active');
-
-        if(arr.length >= td.length) {
-            tr = tr.times(arr.length - td.length);
-            $('.table__bookmark--exchange').find('tbody').append(tr);
-        }
-        else {
-            $('.table__bookmark--exchange tr').slice(arr.length - td.length).remove();
-        }
-
-        $('.table__bookmark--exchange').find('tbody').find('.align-right').each(function(i) {
-            $(this).html('<a href="" class="underlined">' + arr[i] + '</a>');
-        });
     });
 
     $( "#accordion" ).accordion({
