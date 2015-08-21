95704e4567c5cd5d05b4726dfd58e7db726e98cf
mirtlbu <bubuzoglo@gmail.com>
2015-08-21 11:49:18 +0300

    concated and minified

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
         <title>Title</title>
         <meta name="description" content="">
         <meta name="keywords" content="">
-        <link rel="stylesheet" href="/static/vendors/FancySelect/fancySelect.css" type="text/css">
-        <link rel="stylesheet" href="/static/vendors/jquery-ui-1.11.4.custom/jquery-ui.min.css" type="text/css">
-        <link rel="stylesheet" href="/static/desktop/css/style.css" type="text/css">
+        <link rel="stylesheet" href="/static/desktop/css/style.min.css" type="text/css">
         <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
         <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,10 +26,7 @@
 
         <?php include "footer.php";?>
 
-        <script type="text/javascript" src="/static/vendors/jquery-2.1.4.min.js"></script>
-        <script type="text/javascript" src="/static/vendors/jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="/static/vendors/FancySelect/fancySelect.js"></script>
-        <script type="text/javascript" src="/static/desktop/js/script.js"></script>
+        <script type="text/javascript" src="/static/desktop/js/script.min.js"></script>
 
     </body>
 </html>
