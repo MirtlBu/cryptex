b8164f81d9712ac910ed7faed38828f64d03444e
mirtlbu <bubuzoglo@gmail.com>
2016-01-31 13:39:32 +0300

    320 mediaquery

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,8 @@ var cssfiles = [
     'static/desktop/css/start.css',
     'static/desktop/css/verification.css',
     'static/desktop/css/converting.css',
-    'static/desktop/css/dev-style.css'
+    'static/desktop/css/dev-style.css',
+    'static/desktop/css/320.css'
 ];
 
 var jsfiles = [
